e41136f7b9c9e8ab3bf71af82e0b385c04b60a97
Arlen <arlenrww@gmail.com>
2022-07-20 19:00:55 +0600

    v4

--- src/App.js
+++ src/App.js
@@ -1,28 +1,61 @@
+import { useState } from "react";
+
 import Header from "./components/Header/Header";
 import PostForm from "./components/PostForm/PostForm";
 import PostList from "./components/PostList/PostList";
-
-import { useState } from "react";
+import Input from "./components/UI/Input/Input";
+import Select from "./components/UI/Select/Select";
 
 import "./style/app.css";
 
 const App = () => {
   const [posts, setPosts] = useState([]);
+  const [selectedSort, setSelectedSort] = useState("");
 
   const createPost = (post) => {
     post.id = Date.now();
     setPosts([...posts, post]);
   };
 
-  const deletedPost = (id) => {
-    setPosts(posts.filter((p) => p.id !== id));
+  const deletedPost = (post) => {
+    setPosts(posts.filter((p) => p.id !== post.id));
+  };
+
+  const sortPost = (sort) => {
+    setSelectedSort(sort);
+    setPosts([...posts].sort((a, b) => a[sort].localeCompare(b[sort])));
   };
   return (
     <div>
       <Header />
       <div className="wrapper">
         <PostForm create={createPost} />
-        <PostList posts={posts} deleted={deletedPost} />
+        <div className="filterComponent">
+          <Input />
+          <Select
+            onChange={sortPost}
+            value={selectedSort}
+            defaultValue="Сортировка"
+            options={[
+              { value: "title", name: "По заголовку" },
+              { value: "body", name: "По описанию" },
+            ]}
+          />
+        </div>
+        {posts.length > 0 ? (
+          <PostList posts={posts} deleted={deletedPost} />
+        ) : (
+          <h2
+            style={{
+              textAlign: "center",
+              fontWeight: 900,
+              fontSize: "30px",
+              color: "#1e212c",
+            }}
+          >
+            Список пуст
+          </h2>
+        )}
       </div>
     </div>
   );
